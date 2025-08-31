34418b1ed06c5226b4fd4d0eabde6971d97bb9a4
LIZARANIPRIDA <250320100064@cutm.ac.in>
2025-08-31 20:44:23 +0530

    Create Question8.c

---
+++ Question8.c
@@ -0,0 +1,20 @@
+include<stdio.h>
+int main(){
+  int a,b;
+//input the value 
+ printf ("Enter the two numbers: ");
+ scanf("%d %d",&a,&b);
+//Additiona(+)
+add=a+b;
+//Subtraction (-)
+sub=a-b;
+//Multiplication (*)
+multi=a*b;
+//Division (/)
+divis=a/b;
+printf ("Additional=%d\n",add);
+printf ("Subtraction=%d\n",sub);
+printf ("Multiplication=%d\n", multi);
+printf ("Division=%f\n",divis);
+return 0;
+}
