590cc04a1713c2749b0952fd7413ee7814eb3b0d
LIZARANIPRIDA <250320100064@cutm.ac.in>
2025-08-31 22:27:36 +0530

    Update Question9.c

--- Question9.c
+++ Question9.c
@@ -3,15 +3,15 @@ int main (){
   int a,b,c, largest;
 //input the value 
 printf ("Enter the number: ");
-scanf("%d %d %d",&a,&b,%c);
+scanf("%d %d %d",&a,&b,&c);
 if(a >=b && a >= c) {
-  largest = a;
+  printf ("Largest:%d.\n",a);
 }
 else if (b >= a && b >= c){
-  largest = b;
+  printf ("Largest:%d.\n",b);
 }
 else {
-  largest = c;
+  printf ("Largest:%d.\n",c);
 }
 printf ("Largest=%d\n". largest);
 return 0;
