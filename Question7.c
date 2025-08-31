4e090dc6a5c43367c5d71b138491ec4014460421
LIZARANIPRIDA <250320100064@cutm.ac.in>
2025-08-31 21:29:31 +0530

    Update Question7.c

--- Question7.c
+++ Question7.c
@@ -3,7 +3,7 @@ int main (){
   int a,b,c,sum,average;
 //input the number:
 printf ("Enter the three numbers: ");
-scanf("%d",&a,&b,&c);
+scanf("%d %d %d",&a,&b,&c);
 //calculat the sum:
 sum=a+b+c;
 //calculat the average:
