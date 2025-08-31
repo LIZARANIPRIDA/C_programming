cc3ae9c867d905aa7b2218c6ab6677a9a54dd11f
LIZARANIPRIDA <250320100064@cutm.ac.in>
2025-08-31 16:20:40 +0530

    Update Question1.c

--- Question1.c
+++ Question1.c
@@ -1,13 +1,6 @@
 #include<stdio.h>
 int main(){
-  int a,b;
-    printf ("Enter two numbers; ");
-    scanf("%d %d",&a,&b);
-    printf ("Before swapping:a=%d,b=%d\n",a,b);
-      a=a+b;
-      b=a-b;
-      a=a-b;
-    printf ("Before swapping:a=%d,b=%d\n",a,b);
-    return 0;
+  printf ("Hello world");
+  return 0;
 }
    
