bd056595405cfdd0840cf23d937c106015f48d7d
LIZARANIPRIDA <250320100064@cutm.ac.in>
2025-08-31 20:15:01 +0530

    Update Question7.c

--- Question7.c
+++ Question7.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 int main (){
-  int a,b,c,sum; 
-  float average;
+  int a,b,c,sum,average;
 //input the number:
 printf ("Enter the three numbers: ");
 scanf("%d",&a,&b,&c);
@@ -10,7 +9,7 @@ sum=a+b+c;
 //calculat the average:
 average=sum/3;
 printf ("Sum=%d\n",sum);
-printf ("Average=%.2f\n", average);
+printf ("Average=%d\n", average);
 
 return 0;
 }
