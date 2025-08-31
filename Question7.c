55125a626dcfe8d7e44414c7172bb2849395a2fb
LIZARANIPRIDA <250320100064@cutm.ac.in>
2025-08-31 19:57:59 +0530

    Update Question7.c

--- Question7.c
+++ Question7.c
@@ -1,1 +1,11 @@
+#include<stdio.h>
+int main (){
+  int,a,b,c,sum,average;
+//input the number:
+printf ("Enter the three numbers: ");
+scanf("%d",&a,&b,&c);
+//calculat the sum:
+sum=a+b+c;
+//calculat the average:
+average 
 
