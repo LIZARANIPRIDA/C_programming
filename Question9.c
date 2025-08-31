f41771b5ea758474aa7d60cde674dd35a90bd48b
LIZARANIPRIDA <250320100064@cutm.ac.in>
2025-08-31 22:33:52 +0530

    Update Question9.c

--- Question9.c
+++ Question9.c
@@ -5,13 +5,13 @@ int main (){
 printf ("Enter the number: ");
 scanf("%d %d %d",&a,&b,&c);
 if(a >=b && a >= c) {
-  printf ("Largest:%d.\n",a);
+  printf ("Largest:%d\n",a);
 }
 else if (b >= a && b >= c){
-  printf ("Largest:%d.\n",b);
+  printf ("Largest:%d\n",b);
 }
 else {
-  printf ("Largest:%d.\n",c);
+  printf ("Largest:%d\n",c);
 }
 return 0;
 }
