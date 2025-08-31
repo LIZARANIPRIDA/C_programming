5ec02c93bf78affd58af9adcc0c1aaac8309dba6
LIZARANIPRIDA <250320100064@cutm.ac.in>
2025-08-31 22:16:51 +0530

    Create Question9.c

---
+++ Question9.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main (){
+  int a,b,c, largest;
+//input the value 
+printf ("Enter the number: ");
+scanf("%d %d %d",&a,&b,%c);
+if(a >=b && a >= c) {
+  largest = a;
+}
+else if (b >= a && b >= c){
+  largest = b;
+}
+else {
+  largest = c;
+}
+printf ("Largest=%d\n". largest);
+return 0;
+}
+
+  
