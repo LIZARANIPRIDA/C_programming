aa244bc19d136dbe199b11febb7ebf01560f52a6
LIZARANIPRIDA <250320100064@cutm.ac.in>
2025-08-31 17:45:03 +0530

    Update Question4.c

--- Question4.c
+++ Question4.c
@@ -1,1 +1,11 @@
-
+#include<stdio.h>
+int main (){
+char name;
+int age;
+float Mark;
+  printf ("Enter your name");
+  scanf("%c",&name);
+  printf("Enter your age");
+  scanf("%d",&age);
+  printf ("Enter your mark");
+//Displaying 
