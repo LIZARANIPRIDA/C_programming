9606b8fe8835d8ca8d2eeceb8c63304fd82a0d38
LIZARANIPRIDA <250320100064@cutm.ac.in>
2025-08-31 19:22:02 +0530

    Update Question6.c

--- Question6.c
+++ Question6.c
@@ -1,1 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
+
+#define PI 3.14159
+
+int main() {
+    const int radius= 7;  
+    float area;
+    area= PI*radius*radius;
+
+    printf("The value of PI is: %.5f\n", PI);
+    printf("There are %d radius.\n", radius);
+
+    return 0;
+}
