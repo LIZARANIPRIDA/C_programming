660bbf658e496ad6854dbf119d19a3bbcfa14e0e
LIZARANIPRIDA <250320100064@cutm.ac.in>
2025-09-05 14:13:45 +0530

    Update Question14.c

--- Question14.c
+++ Question14.c
@@ -4,4 +4,11 @@ int main (){
 printf ("Enter a number: ");
 scanf ("%d %d",&a,&b);
 car(a==0&&b==0);
-printf ()
+printf ("(a==0&&b==0)\n");
+car (a>b||b>a);
+printf ("(a>b||b>a)\n");
+car (a!=0);
+printf ("(a!=0)\n");
+return 0;
+}
+
